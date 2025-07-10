0c6ff0c4bbfa3458a3f99b34f5afb18df076aaf8
sahinsyed63 <sahin.syed63@gmaol.com>
2025-07-10 23:12:40 +0530

    Initial commit of Spring Boot project

---
+++ failure-analyzer-project/src/main/java/com/example/failure_analyzer_project/FailureAnalyzerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.failure_analyzer_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FailureAnalyzerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FailureAnalyzerProjectApplication.class, args);
+	}
+
+}
---
+++ failure-analyzer-project/src/test/java/com/example/failure_analyzer_project/FailureAnalyzerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.failure_analyzer_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FailureAnalyzerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
